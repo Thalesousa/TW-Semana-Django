34fc3064ceb39b03c4a4ef33143664bd0d9cda16
thalesousa <thalestjsb@gmail.com>
2019-09-25 23:59:57 -0300

    adicionando models e forms

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .forms import TarefaForm
 
 def listar_tarefas(request):
     nome_tarefa = 'Assistir a semana de python e django'
     return render(request,'tarefas/listar_tarefas.html', {'nome_tarefa': nome_tarefa})
+
+
+def cadastrar_tarefas(request):
+    form_tarefa = TarefaForm
+    return render(request, 'tarefas/form_tarefa.html',{'form_tarefa': form_tarefa})
